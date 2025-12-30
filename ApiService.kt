f221ed8772c01c4622e25f08939751cf738a9bca
TUSHAR AMBAJI <tusharambaji@creatiftechnologies.com>
2025-12-31 00:47:09 +0530

    Add files via upload

---
+++ ApiService.kt
@@ -0,0 +1,13 @@
+package network
+
+import retrofit2.Retrofit
+
+import retrofit2.convert.gson.GsonConvertrFactory
+import kotlin.random.Random
+
+object ApisWeber {
+    private const val BASE_URL = "https/:"
+
+    val api: ApiService by lazy {
+    }
+}
